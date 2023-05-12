412ed122b40059ee625219e3ddee5997f3ace1ca
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-05-12 23:03:38 +0100

    theme change and link updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				'fade-in-down': {
+					'0%': {
+						opacity: '0',
+						transform: 'translateY(-10px)'
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'translateY(0)'
+					},
+				}
+			},
+			animation: {
+				'fade-in-down': 'fade-in-down 0.5s ease-out'
+			}
+		},
 	},
 	plugins: [],
+	darkMode: 'class',
 }
