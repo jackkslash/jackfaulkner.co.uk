ca6aac646e8b2fa6ca73d18428e68fb1768cc80b
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-05-13 01:35:14 +0100

    Added project data hardcoded and added transition

---
+++ src/lib/projects.ts
@@ -0,0 +1,33 @@
+export interface Project {
+  title: string;
+  description: string;
+  href?: string;
+}
+
+export const ProjectData: Project[] = [
+  {
+    title: "F1Stats",
+    description: "F1 Statistics and data site built with react.",
+    href: "https://github.com/jackkslash/F1Stats",
+  },
+  {
+    title: "jackfaulkner.co.uk",
+    description: "Personal site built in astro.",
+    href: "https://github.com/jackkslash/jackfaulkner.co.uk",
+  },
+  {
+    title: "MSAI",
+    description: "Meme reverse image search using vector database and AI.",
+    href: "https://github.com/jackkslash/MSAI",
+  },
+  {
+    title: "Scripts",
+    description: "Utility Bash Scripts",
+    href: "https://github.com/jackkslash/scripts",
+  },
+  {
+    title: "Arceus",
+    description: "Q and A tool for Twitch.",
+    href: "https://github.com/jackkslash/Arceus",
+  },
+];
