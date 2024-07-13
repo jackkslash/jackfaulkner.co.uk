ef3fcaea6e51c6b1090d2f2666a8b32f115146b7
jackkslash <jackfaulknerbis@hotmail.co.uk>
2024-07-13 22:55:05 +0100

    favicon update, icons added, link to blog readded

--- src/lib/icons.tsx
+++ src/lib/icons.tsx
@@ -17,3 +17,22 @@ export function UpRightArrowIcon() {
         </svg>
     );
 }
+
+export function LocationIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-location">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" />
+        </svg>
+    );
+}
+
+export function JobIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-briefcase-2">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 9a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9z" />
+            <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2" />
+        </svg>
+    );
+}   
